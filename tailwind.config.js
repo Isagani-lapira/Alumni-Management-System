870b0ae945468c90538ec322c5cf192d6af79601
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-04-10 08:15:02 +0800

    feat: Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./**/*.{html,js}"],
   theme: {
+    // allows us to use custom colors
+    colors: {
+      accent: "#991B1B",
+      secondary: "#1746A2",
+    },
     extend: {},
   },
   plugins: [],
