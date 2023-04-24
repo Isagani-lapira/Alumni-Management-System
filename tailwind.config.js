0811723815507be85e3d2eb505389ec724e797f7
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-04-24 15:39:43 +0800

    feat: Add Montserrat as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+    },
     // allows us to use custom colors
     extend: {
       colors: {
