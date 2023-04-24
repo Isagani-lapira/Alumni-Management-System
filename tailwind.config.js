b9c8c0cb5009ba3bb6aa69d59a209b95ae96c211
Wil Miranda <70352471+WilAm1@users.noreply.github.com>
2023-04-24 15:50:03 +0800

    Landing Page Style  (#1)
    * feat: Add minified version in build script
    
    * fix: color must be extending
    
    * Add structure until programs and events
    
    * fix: Adjust css"
    
    * feat: Add donation
    
    * feat: Add partial for calendar
    
    * feat: Add css to footer
    
    * feat: Add style to calendar"
    
    * feat: Adjut css to upper part
    
    * feat: Add Montserrat as default font
    
    * refactor: Add comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    // allows us to use custom colors
-    colors: {
-      accent: "#991B1B",
-      secondary: "#1746A2",
+    fontFamily: {
+      sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
     },
-    extend: {},
+    // allows us to use custom colors
+    extend: {
+      colors: {
+        accent: "#991B1B",
+        secondary: "#1746A2",
+        grayish: "#666666",
+      },
+    },
   },
   plugins: [],
 };
