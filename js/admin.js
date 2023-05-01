1fbb85a73f92e5ad4e0b84fe604b0a5151d62221
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-02 06:41:48 +0800

    changing tabs: Alumni

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,7 @@ const defaultStyle = "text-black";
 
 $(document).ready(()=>{
   $(".dashboard").addClass(classStyle);
+  $(".dashboard svg").css("fill","#f6f6f6");
 })
 
 // hove effect in side bar
@@ -23,9 +24,39 @@ $(document).ready(()=>{
     $(".sidebar div").removeClass(classStyle).addClass(defaultStyle);
     $(this).addClass(classStyle)
 
-    //change icon color
+
+    //change icon color active/not
+    $(".sidebar div svg").css("fill","#474645"); //set as default color
+
+    //make the selected color as active button
     let icon = $(this).find('svg').attr("id");
     let iconVar = document.getElementById(icon);
     iconVar.style.fill = "#f6f6f6"
+
+    let sideBar = $(this).find('span').html();
+    visibleContent(sideBar);
   })
 })
+
+const visibleContent = (item)=>{
+  
+  const mainDiv = document.querySelectorAll(".content");
+
+  mainDiv.forEach(child =>{
+    child.style.display = "none";
+  })
+
+  let sideBarID;
+  switch(item){
+    case 'DASHBOARD':
+      console.log("somethings wrong");
+      break;
+    case 'ANNOUNCEMENT':
+      sideBarID = 'announcement-content';
+      break;
+  }
+
+  //display the tab content
+  document.getElementById(sideBarID).classList.toggle("hidden");
+  document.getElementById(sideBarID).style.display = "block";
+}
