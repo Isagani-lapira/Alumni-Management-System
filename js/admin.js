d1c5d390badfd398ed98d331171d550bef4e429a
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-29 12:40:17 +0800

    Adding tabs
    Adding announcement and reports tabs. Also, I added a highlight effect where everytime the active tab is on its text and icon will be highlighted from the rest

--- js/admin.js
+++ js/admin.js
@@ -1,79 +1,11 @@
-const classStyle = "bg-accent text-white font-bold";
-const defaultStyle = "text-black";
+$(document).ready(function(){
+  $("#tabs").tabs();
 
-$(document).ready(()=>{
-  $(".dashboard").addClass(classStyle);
-  $(".dashboard svg").css("fill","#f6f6f6");
-})
 
-// hove effect in side bar
-$(document).ready(function() {
-    const additionalClass = "font-extrabold text-accent text-10xs";
-    $('.sidebar span').hover(function() {
-      $(this).addClass(additionalClass);
-    }, function() {
-      $('.sidebar span').removeClass(additionalClass);
-    });
-});
-
-// change view
-$(document).ready(()=>{
-  $('.sidebar div').click(function(){
+  //change the tab appearance when active and not
+  $(".tabs li").click(function(){
+    $(".tabs li").removeClass("ui-tabs-active")
+    $(this).addClass("ui-tabs-active")
     
-    //change the menu to active
-    $(".sidebar div").removeClass(classStyle).addClass(defaultStyle);
-    $(this).addClass(classStyle)
-
-
-    //change icon color active/not
-    $(".sidebar div svg").css("fill","#474645"); //set as default color
-
-    //make the selected color as active button
-    let icon = $(this).find('svg').attr("id");
-    let iconVar = document.getElementById(icon);
-    iconVar.style.fill = "#f6f6f6"
-
-    let sideBar = $(this).find('span').html();
-    visibleContent(sideBar);
   })
-})
-
-
-const visibleContent = (item)=>{
-  
-  const mainDiv = document.querySelectorAll(".content");
-
-  mainDiv.forEach(child =>{
-    child.style.display = "none";
-  })
-
-
-  let sideBarID;
-
-  //check which tab is selected
-  if(item==='DASHBOARD')
-    console.log('pumasok na')
-  else if(item==='ANNOUNCEMENT')
-    sideBarID = 'announcement-content'
-  else if(item==='REPORT')
-    sideBarID = 'report-content'
-
-
-  //display the tab content
-  document.getElementById(sideBarID).classList.toggle("hidden");
-  document.getElementById(sideBarID).style.display = "block";
-}
-
-
-
-//get year from 2000 to current year
-let batchOption = document.getElementById('batch');
-
-let currentYr = new Date().getFullYear();
-for(i = currentYr; i>=2000; i--){
-  let newOption = document.createElement('option');
-  newOption.value = i;
-  newOption.text = i;
-  console.log("pumasok")
-  batchOption.appendChild(newOption);
-}
+});
