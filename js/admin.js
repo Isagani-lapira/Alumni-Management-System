8ebd334832a609f06dca8980df264043ce9bc7d9
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-30 14:51:48 +0800

    Adding graphs
    Adding graphs for tracer status and response by year

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,71 @@
-$(document).ready(function(){
+
+const redAccent = '#991B1B'
+const blueAccent = '#2E59C6'
+
+$(document).ready(function () {
   $("#tabs").tabs();
 
 
   //change the tab appearance when active and not
-  $(".tabs li").click(function(){
+  $(".tabs li").click(function () {
     $(".tabs li").removeClass("ui-tabs-active")
     $(this).addClass("ui-tabs-active")
-    
+
   })
 });
+
+//chart function
+const tracerStatus = document.getElementById('myChart');
+const tracerType = 'pie'
+const tracerLabels = ["Already answered", "Haven't answer yet"]
+const tracerData = [12, 1]
+const color = [blueAccent, redAccent]
+
+chartConfig(tracerStatus, tracerType, tracerLabels,
+  tracerData, true, color)
+
+
+//chart for response by year
+const responseByYear = document.getElementById('responseByYear')
+const responseByYear_labels = ["2021", "2020", "2019"]
+const responseByYear_data = [1000, 500, 247]
+const responseByYear_type = 'bar'
+chartConfig(responseByYear, responseByYear_type, responseByYear_labels,
+  responseByYear_data, false, redAccent)
+
+
+function chartConfig(chartID, type, labels, data, responsive, colors) {
+  //the chart
+  new Chart(chartID, {
+    type: type,
+    data: {
+      labels: labels,
+      datasets: [{
+        backgroundColor: colors,
+        data: data,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: responsive, // Disable responsiveness
+      maintainAspectRatio: false, // Disable aspect ratio
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      plugins: {
+        legend: {
+          display: true,
+          position: 'bottom',
+          labels: {
+            font: {
+              weight: 'bold'
+            }
+          },
+
+        }
+      }
+    }
+  });
+}
