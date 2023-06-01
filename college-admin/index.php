2a354bce81b4b4d4fc90535d6113e6dbb4b9eea1
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-01 15:07:39 +0800

    feat: add sidebar html

--- college-admin/index.php
+++ college-admin/index.php
@@ -5,10 +5,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>Dashboard</title>
 </head>
 
 <body>
+
+    <aside>
+        <nav>
+            <ul>
+                <li><a href="#dashboard">Dashboard</a></li>
+                <li><a href="#announcements">Announcements</a></li>
+                <li><a href="#reports">Reports</a></li>
+                <li><a href="#event">Event</a></li>
+                <li><a href="#forms">Forms</a></li>
+                <li><a href="#profile">Profile</a></li>
+            </ul>
+            <div>Alumni</div>
+            <ul>
+                <li><a href="#alumni-of-the-month">Alumni of the Month</a></li>
+                <li><a href="#community">Communitity</a></li>
+                <li><a href="#jobs">Job Oppurtunities</a></li>
+            </ul>
+        </nav>
+    </aside>
+
+    <main id="main-root">
+
+    </main>
 
 </body>
 
