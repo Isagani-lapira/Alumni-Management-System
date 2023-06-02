08c980ce775d91f87809678fc11ad760eacf2d01
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-02 15:15:15 +0800

    feat: Add pages to pages account

--- college-admin/index.php
+++ college-admin/index.php
@@ -43,21 +43,21 @@
             <!-- TODO add icons -->
             <nav class="">
                 <ul>
-                    <li><a href="#dashboard" class="flex">
+                    <li><a href="#dashboard" class="flex rounded-lg p-2  ">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chart-pie-filled" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M9.883 2.207a1.9 1.9 0 0 1 2.087 1.522l.025 .167l.005 .104v7a1 1 0 0 0 .883 .993l.117 .007h6.8a2 2 0 0 1 2 2a1 1 0 0 1 -.026 .226a10 10 0 1 1 -12.27 -11.933l.27 -.067l.11 -.02z" stroke-width="0" fill="currentColor" />
                                 <path d="M14 3.5v5.5a1 1 0 0 0 1 1h5.5a1 1 0 0 0 .943 -1.332a10 10 0 0 0 -6.11 -6.111a1 1 0 0 0 -1.333 .943z" stroke-width="0" fill="currentColor" />
                             </svg>
                             Dashboard</a></li>
-                    <li><a href="#announcements" class="flex">
+                    <li><a href="#announcements" class="flex rounded-lg p-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-messages" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
                                 <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
                             </svg>
                             Announcements</a></li>
-                    <li><a href="#reports" class="flex">
+                    <li><a href="#reports" class="flex rounded-lg p-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-timeline" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M4 16l6 -7l5 5l5 -6" />
@@ -67,7 +67,7 @@
                                 <path d="M20 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
                             </svg>
                             Reports</a></li>
-                    <li><a href="#event" class="flex ">
+                    <li><a href="#event" class="flex rounded p-2 ">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-calendar-stats" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M11.795 21h-6.795a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v4" />
@@ -78,14 +78,14 @@
                                 <path d="M3 11h16" />
                             </svg>
                             Event</a></li>
-                    <li><a href="#forms" class="flex">
+                    <li><a href="#forms" class="flex rounded p2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
                                 <path d="M13.5 6.5l4 4" />
                             </svg>
                             Forms</a></li>
-                    <li><a href="#profile" class="flex">
+                    <li><a href="#profile" class="flex rounded p2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
@@ -97,7 +97,7 @@
                 <div>Alumni</div>
                 <ul>
                     <li><a href="#alumni-of-the-month" class="
-                    flex">
+                    flex rounded p2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-medal-2" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M9 3h6l3 7l-6 2l-6 -2z" />
@@ -106,7 +106,7 @@
                                 <path d="M12 19.5l-3 1.5l.5 -3.5l-2 -2l3 -.5l1.5 -3l1.5 3l3 .5l-2 2l.5 3.5z" />
                             </svg>
                             Alumni of the Month</a></li>
-                    <li><a href="#community" class="flex">
+                    <li><a href="#community" class="flex rounded p2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users-group" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
@@ -117,7 +117,7 @@
                                 <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
                             </svg>
                             Communitity</a></li>
-                    <li><a href="#jobs" class="flex">
+                    <li><a href="#jobs" class="flex rounded p2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-briefcase" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
@@ -148,6 +148,9 @@
 
             // manages the click of links
             $('nav li a').click(function(e) {
+                $('nav li a').removeClass("font-bold bg-darkAccent text-white");
+                $(this).addClass("font-bold bg-darkAccent text-white");
+
                 const fullPageName = e.target.href;
                 // gets the name of the link
                 const section = fullPageName.split('#')[1];
