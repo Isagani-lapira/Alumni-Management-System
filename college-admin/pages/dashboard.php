ba1db2f72455d8dced43f5082ac66fc385ee2e0e
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-02 14:28:10 +0800

    feat: Configure chart.js bar graph

--- college-admin/pages/dashboard.php
+++ college-admin/pages/dashboard.php
@@ -1,5 +1,5 @@
 <!-- dashboard content -->
-<div id="dashboard-tab">
+<div id="dashboard-tab" class="container mx-auto">
     <h1 class="text-xl font-extrabold">DASHBOARD</h1>
 
     <button id="btnAnnouncement" style="margin-left: auto; margin-right: 10px" class="block rounded-lg font-bold text-white bg-accent p-2 hover:bg-darkAccent">
@@ -22,7 +22,7 @@
             <div class="flex flex-wrap columns-3 mt-2 gap-2">
                 <!-- total user -->
                 <div class="dash-content flex-1 rounded-lg p-2 relative border">
-                    <img class="inline" src="/images/check-icon.svg" alt="" />
+                    <i class="bi bi-check-circle-fill"></i>
                     <span class="text-textColor text-xs font-bold text relative">
                         TOTAL USERS
                         <br />
@@ -107,9 +107,73 @@
     <!-- Response by year -->
     <div class="m-10 max-lg: relative pb-3 font-semibold">
         <p class=" mb-2">RESPONSE BY YEAR </p>
-        <div class="response_by_year">
-            <canvas class="block mx-auto" style="height:40vh; width:40vw" id="responseByYear"></canvas>
+        <div class="w-2/3 h-96 mx-auto">
+            <canvas class="" id="responseByYear"></canvas>
         </div>
 
     </div>
 </div>
+
+<script>
+    $(document).ready(() => {
+
+        const redAccent = '#991B1B'
+        const blueAccent = '#2E59C6'
+
+        //chart function
+        const tracerStatus = document.getElementById('myChart');
+        const tracerType = 'pie'
+        const tracerLabels = ["Already answered", "Haven't answer yet"]
+        const tracerData = [12, 1]
+        const color = [blueAccent, redAccent]
+
+        chartConfig(tracerStatus, tracerType, tracerLabels,
+            tracerData, true, color)
+
+
+        //chart for response by year
+        const responseByYear = document.getElementById('responseByYear')
+        const responseByYear_labels = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014"]
+        const responseByYear_data = [1000, 500, 247, 635, 323, 393, 290, 860]
+        const responseByYear_type = 'bar'
+        chartConfig(responseByYear, responseByYear_type, responseByYear_labels,
+            responseByYear_data, true, redAccent)
+
+
+        function chartConfig(chartID, type, labels, data, responsive, colors) {
+            //the chart
+            new Chart(chartID, {
+                type: type,
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        backgroundColor: colors,
+                        data: data,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: true,
+                            position: 'bottom',
+                            labels: {
+                                font: {
+                                    weight: 'bold'
+                                }
+                            },
+
+                        }
+                    }
+                }
+            });
+        }
+    })
+</script>
