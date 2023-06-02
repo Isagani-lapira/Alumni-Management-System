fb986a46c530603785a36a68303b7f3882bd8c66
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-02 14:22:31 +0800

    feat: Add lodash package

--- college-admin/index.php
+++ college-admin/index.php
@@ -24,6 +24,9 @@
     <!-- Chart JS -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+    <!-- Lodash Utility Library -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 
 <body class="">
