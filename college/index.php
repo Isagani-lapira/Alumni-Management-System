d6b465a0ba80ccb18405948e3f415d813080c6de
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-03 14:24:32 +0800

    feat: Add missing pages

--- college/index.php
+++ college/index.php
@@ -120,7 +120,7 @@
                                 <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
                             </svg>
                             Communitity</a></li>
-                    <li><a href="#jobs" class="flex rounded p-2">
+                    <li><a href="#job-oppurtunities" class="flex rounded p-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-briefcase" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
