cc78c303430eb05e9157284730585c76b1dfc275
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-04 11:17:38 +0800

    feat: Add html content to forms

---
+++ college/pages/forms.php
@@ -0,0 +1,15 @@
+<h1>Alumni Tracer Form</h1>
+<p>Here you can see the list of alumni that answers the form</p>
+
+<button>Deploy Tracer</button>
+
+<hr>
+
+<!-- Pie Chart -->
+
+<h3>Employee Status</h3>
+<!-- TODO add chart here -->
+<h3>Annual Salary</h3>
+<!-- TODO add chart here -->
+<h3>Relevant Skills Developed</h3>
+<!-- TODO add chart here -->
