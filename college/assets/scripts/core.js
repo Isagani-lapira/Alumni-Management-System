6313e24c53ff21b44cda0b21df16e5a362058a60
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-06-14 13:57:03 +0800

    refactor: Change div element to section element

---
+++ college/assets/scripts/core.js
@@ -0,0 +1,83 @@
+// TODO Refactor later when doing backend
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+$(document).ready(function () {
+  // detect hashed id in the first load
+  if (window.location.hash) {
+    const linkName = window.location.hash.substr(1);
+    // change the color of the links
+    handleLinkFocusChange(linkName);
+    // change the url
+    loadURL(linkName, $("#main-root"));
+  }
+  // manages link clicks
+  $("nav li a").click(function (e) {
+    const link = e.target.getAttribute("data-link");
+    handleLinkFocusChange(link);
+    loadURL(link, $("#main-root"));
+  });
+
+  function handleLinkFocusChange(pageName) {
+    // changes the state of nav links to highlight focus
+    const elem = $(`nav li a[data-link="${pageName}"`);
+    $("nav li a").removeClass("font-bold bg-darkAccent text-white");
+    elem.addClass("font-bold bg-darkAccent text-white");
+  }
+
+  function loadURL(url, container, title = "") {
+    // change the root elem to page
+
+    const base_url = "pages/";
+    title = title.length === 0 ? url : title;
+    // set the title of the page
+    document.title = capitalizeFirstLetter(title);
+    $.ajax({
+      type: "GET",
+      url: base_url + url + ".php",
+      dataType: "html",
+      // TODO check later if cache is good in changing data
+      cache: true,
+      beforeSend: function () {
+        // add loader while waiting
+        // TODO make better loading screen
+        container.html("<h1>Loading...</h1>");
+
+        // scroll to top
+        if (container[0] === $(".main-root")[0]) {
+          $("html").animate(
+            {
+              scrollTop: 0,
+            },
+            "fast"
+          );
+        }
+      },
+      complete: function (res) {
+        //
+        if (title != "") document.title = capitalizeFirstLetter(title);
+      },
+      success: function (data) {
+        // animate a bit
+        container
+          .css({
+            opacity: "0.0",
+          })
+          .html(data)
+          .delay(50)
+          .animate(
+            {
+              opacity: "1.0",
+            },
+            300
+          );
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        // No Page Found
+        container.load("pages/missing-page.php");
+      },
+      async: false,
+    });
+  }
+});
--- college/index.php
+++ college/index.php
@@ -27,7 +27,10 @@
     <!-- Lodash Utility Library -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    <!-- App Script -->
+    <script src="./assets/scripts/core.js" defer></script>
     <!-- End JS Scripts -->
+
 </head>
 
 <body class="">
@@ -141,87 +144,6 @@
         </main>
     </div>
 
-    <script>
-        // TODO Refactor later when doing backend
-        function capitalizeFirstLetter(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        }
-
-        $(document).ready(function() {
-
-            // detect hashed id in the first load
-            if (window.location.hash) {
-                const linkName = window.location.hash.substr(1);
-                // change the color of the links
-                handleLinkFocusChange(linkName)
-                // change the url
-                loadURL(linkName, $('#main-root'));
-            }
-            // manages link clicks
-            $('nav li a').click(function(e) {
-                const link = e.target.getAttribute('data-link')
-                handleLinkFocusChange(link)
-                loadURL(link, $('#main-root'))
-            })
-
-
-
-
-            function handleLinkFocusChange(pageName) { // changes the state of nav links to highlight focus
-                const elem = $(`nav li a[data-link="${pageName}"`);
-                $('nav li a').removeClass("font-bold bg-darkAccent text-white");
-                elem.addClass("font-bold bg-darkAccent text-white");
-
-            }
-
-
-
-            function loadURL(url, container, title = '') { // change the root elem to page
-
-                const base_url = 'pages/'
-                title = title.length === 0 ? url : title;
-                // set the title of the page
-                document.title = capitalizeFirstLetter(title);
-                $.ajax({
-                    type: "GET",
-                    url: base_url + url + '.php',
-                    dataType: 'html',
-                    // TODO check later if cache is good in changing data 
-                    cache: true,
-                    beforeSend: function() {
-                        // add loader while waiting
-                        // TODO make better loading screen
-                        container.html('<h1>Loading...</h1>');
-
-                        // scroll to top
-                        if (container[0] === $(".main-root")[0]) {
-                            $("html").animate({
-                                scrollTop: 0
-                            }, "fast");
-                        }
-                    },
-                    complete: function(res) {
-                        // 
-                        if (title != '') document.title = capitalizeFirstLetter(title);
-                    },
-                    success: function(data) {
-                        // animate a bit
-                        container.css({
-                            opacity: '0.0'
-                        }).html(data).delay(50).animate({
-                            opacity: '1.0'
-                        }, 300);
-                    },
-                    error: function(xhr, ajaxOptions, thrownError) {
-                        // No Page Found
-                        container.load("pages/missing-page.php");
-                    },
-                    async: false
-                });
-            }
-
-        })
-    </script>
 
 </body>
 
