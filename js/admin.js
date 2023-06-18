433f9002cae420de60f89fb725a4a64c2e88322f
Isagani-lapira <lapiraisagani@gmail.com>
2023-06-18 08:52:12 +0800

    Date range functionality

--- js/admin.js
+++ js/admin.js
@@ -198,6 +198,30 @@ $(document).ready(function () {
     });
   });
 
+  $(function () {
+    $('input[name="emDateRange"]').daterangepicker({
+      opens: 'left'
+    }, function (start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+  });
+
+  $(function () {
+    $('input[name="reportdaterange"]').daterangepicker({
+      opens: 'left'
+    }, function (start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+  });
+
+  $(function () {
+    $('input[name="aoydaterange"]').daterangepicker({
+      opens: 'left'
+    }, function (start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+  });
+
   $('#aoyNew').on('click', () => {
     $('#aoyRecord').hide()
     $('#aoyRegister').show()
