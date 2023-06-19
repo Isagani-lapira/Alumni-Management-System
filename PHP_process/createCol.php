33ecbca76f20b0c7ec60848f0cc2b954ff2d0022
Isagani-lapira <lapiraisagani@gmail.com>
2023-06-19 17:16:20 +0800

    Resuable Code for Person and College

--- PHP_process/createCol.php
+++ PHP_process/createCol.php
@@ -1,36 +1,59 @@
 <?php
     require_once 'connection.php';
+    require_once 'personDB.php';
     
     $arraData = $_POST['arrayData'];
     $myArray = json_decode($arraData, true);
 
-    $resposeData = [
-        'status'=> 'success',
-        'message'=>'Process successfully',
-        'data'=>$myArray
-    ];
-
-
     //values
-    $colName = $myArray[2];
-    $colCode = $myArray[3];
-    $colEmailAdd = $myArray[4];
-    $colContactNo = $myArray[5];
-    $colWebLink = $myArray[6];
+    $colName = $myArray[1];
+    $colCode = $myArray[2];
+    $colEmailAdd = $myArray[3];
+    $colContactNo = $myArray[4];
+    $colWebLink = $myArray[5];
+    $fname = $myArray[6];
+    $lname = $myArray[7];
+    $personalEm = $myArray[8];
+    $bulsuEm = $myArray[9];
+    $contactNo = $myArray[10];
+    $address = $myArray[11];
+    $birthday = $myArray[12];
+    $gender = $myArray[13];
 
-    $query = "INSERT INTO `college`(`colCode`, `colname`, `colEmailAdd`, 
-    `colContactNo`, `colWebLink`, `colLogo`, `colAdmin`, `colDean`, 
-    `colDeanImg`, `adminImg`) VALUES ('$colCode','$colName','$colEmailAdd',
-    '$colContactNo','$colWebLink',NULL,NULL,NULL,
-    NULL,NULL)";
+    $query = "INSERT INTO `college`(`colCode`, `colname`, 
+    `colEmailAdd`, `colContactNo`, `colWebLink`, `colLogo`,
+    `colDean`, `colDeanImg`) VALUES ('$colCode','$colName','$colEmailAdd',
+    '$colContactNo','$colWebLink',NULL,NULL,NULL)";
 
     $result = mysqli_query($mysql_con,$query);
 
+    // $personID,$FName,$LName,$age,$bday,
+    //$contactNo,$address,$personalEmail,$bulsuEmail,$gender,$profilePic 
     if($result){
-        echo 'Insertion Complete';
+        $currentDate = date('y/m/d');
+        $personID = 'admin'.$currentDate;
+        $person = new personDB();
+        $person->insertPerson($personID,$fname,$lname,getAge($birthday),$birthday,
+                                $contactNo,$address,$personalEm,$bulsuEm,$gender,null,$mysql_con);
     }
     else
         echo 'problem insertions';
 
+    function getAge($bday){
+        // Current date
+        $currentDate = date('Y-m-d');
+
+        // Create DateTime objects for the birthdate and current date
+        $birthdateObj = new DateTime($bday);
+        $currentDateObj = new DateTime($currentDate);
+
+        // Calculate the difference between the two dates
+        $ageInterval = $birthdateObj->diff($currentDateObj);
+
+        // Retrieve the age from the difference
+        $age = $ageInterval->y;
+
+        return $age; 
+    }
     mysqli_close($mysql_con);
 ?>
---
+++ PHP_process/personDB.php
@@ -0,0 +1,19 @@
+<?php
+    class personDB{
+        // insertion data
+        public function insertPerson($personID,$FName,$LName,$age,$bday,
+        $contactNo,$address,$personalEmail,$bulsuEmail,$gender,$profilePic ,$con){
+            
+            $query = "INSERT INTO `person`(`personID`, `fname`, `lname`, 
+            `age`, `bday`, `gender`, `contactNo`, `address`, `personal_email`, 
+            `bulsu_email`, `profilepicture`) VALUES ('$personID','$FName',
+            '$LName','$age','$bday','$contactNo','$address','$personalEmail',
+            '$bulsuEmail','$gender','$profilePic')";
+
+            $result = mysqli_query($con,$query);
+            
+            if($result) echo 'completed!';
+            else echo 'Cannot be inserted!';
+        }
+    }
+?>
