2c9698e103e2f147562c4956a0d86e59dfcce338
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2023-06-21 13:38:07 +0800

    update

--- student-alumni/js/hompage.js
+++ student-alumni/js/hompage.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
   // Prevent closing the modal when clicking inside it
   $('.modal-container').click(function(e) {
     e.stopPropagation();
-  });
+  }); 
   
 });
 
