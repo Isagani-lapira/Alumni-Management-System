86a1626cb58abbd291cfa9fff2e2a45a47a66d27
Isagani-lapira <lapiraisagani@gmail.com>
2023-07-03 16:01:56 +0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -946,6 +946,21 @@
               <!-- college -->
               <select name="aoyCol" id="aoyCol" class="w-1/2 p-1">
                 <option value="" selected disabled hidden>College</option>
+                <?php
+                require_once '../PHP_process/connection.php';
+                $query = "SELECT * FROM `college`";
+                $result = mysqli_query($mysql_con, $query);
+                $rows = mysqli_num_rows($result);
+
+                if ($rows > 0) {
+                  while ($data = mysqli_fetch_assoc($result)) {
+                    $colCode = $data['colCode'];
+                    $colName = $data['colname'];
+
+                    echo '<option value="' . $colCode . '">' . $colName . '</option>';
+                  }
+                } else echo '<option>No college available</option>';
+                ?>
               </select>
 
             </div>
