97d2843a503309341f16dc080d57936720495d76
Isagani-lapira <lapiraisagani@gmail.com>
2023-07-18 14:39:40 +0800

    job list

--- PHP_process/career.php
+++ PHP_process/career.php
@@ -78,89 +78,16 @@ class Career
         $query = "SELECT * FROM `career` ORDER BY`date_posted`DESC"; //as defult
         $result = mysqli_query($con, $query);
 
-        if ($result) {
+        if ($result) $this->getCareerDetail($result, $con);
+        else echo 'something went wrong, please try again';
+    }
+    public function selectDataForCollege($college, $con)
+    {
+        $query = 'SELECT * FROM `career` WHERE `colCode` ="' . $college . '"';
+        $result = mysqli_query($con, $query);
 
-            $response = "";
-            $careerID = array();
-            $jobTitle = array();
-            $companyName = array();
-            $jobDescript = array();
-            $jobQuali = array();
-            $companyLogo = array();
-            $minSalary = array();
-            $maxSalary = array();
-            $colCode = array();
-            $author = array();
-            $date_posted = array();
-            $skills = array();
-            $requirements = array();
-            $personIDEncrypted = array();
-
-            if (mysqli_num_rows($result) > 0) {
-                $response = "Success";
-                while ($row_data = mysqli_fetch_assoc($result)) {
-                    $careerID[] = $row_data['careerID'];
-                    $jobTitle[] = $row_data['jobTitle'];
-                    $companyName[] = $row_data['companyName'];
-                    $jobDescript[] = $row_data['jobDescript'];
-                    $jobQuali[] = $row_data['jobqualification'];
-                    $companyLogo[] = base64_encode($row_data['companyLogo']);
-                    $minSalary[] = $row_data['minSalary'];
-                    $maxSalary[] = $row_data['colCode'];
-                    $colCode[] = $row_data['colCode'];
-                    $author[] = $row_data['author'];
-                    $date_posted[] = $row_data['date_posted'];
-                    // Pseudonymize the personID
-                    $personIDEncrypted[] = $this->generatePseudonym($row_data['personID']);
-
-                    //retrieve skills from the database
-                    $skillQuery = 'SELECT * FROM `skill` WHERE careerID = "' . $row_data['careerID'] . '"';
-                    $skillResult = mysqli_query($con, $skillQuery);
-                    $skillNames = array();
-
-                    if ($skillResult && mysqli_num_rows($skillResult) > 0) {
-                        while ($skill_data = mysqli_fetch_assoc($skillResult)) {
-                            $skillNames[] = $skill_data['skill'];
-                        }
-                    }
-
-                    $skills[] = $skillNames;
-
-                    //retrieve requirements
-                    $reqQuery = 'SELECT * FROM `requirement` WHERE careerID = "' . $row_data['careerID'] . '"';
-                    $reqResult = mysqli_query($con, $reqQuery);
-                    $requirement = array();
-
-                    if ($reqResult && mysqli_num_rows($reqResult) > 0) {
-                        while ($req_data = mysqli_fetch_assoc($reqResult)) {
-                            $requirement[] = $req_data['requirement'];
-                        }
-                    }
-                    $requirements[] = $requirement;
-                }
-            } else $response = "Error";
-
-            //data to be sent
-            $data =  array(
-                'result' => $response,
-                'careerID' => $careerID,
-                'jobTitle' => $jobTitle,
-                'companyName' => $companyName,
-                'companyLogo' => $companyLogo,
-                'jobDescript' => $jobDescript,
-                'jobQuali' => $jobQuali,
-                'minSalary' => $minSalary,
-                'maxSalary' => $maxSalary,
-                'skills' => $skills,
-                'requirements' => $requirements,
-                'colCode' => $colCode,
-                'author' => $author,
-                'date_posted' => $date_posted,
-                'personID' => $personIDEncrypted,
-            );
-
-            echo json_encode($data); //return data as json
-        } else echo $query;
+        if ($result) $this->getCareerDetail($result, $con);
+        else echo 'something went wrong, please try again';
     }
 
     function generatePseudonym($personID)
@@ -168,5 +95,89 @@ class Career
         $pseudonym = md5($personID);
 
         return $pseudonym;
+    }
+
+    function getCareerDetail($result, $con)
+    {
+        $response = "";
+        $careerID = array();
+        $jobTitle = array();
+        $companyName = array();
+        $jobDescript = array();
+        $jobQuali = array();
+        $companyLogo = array();
+        $minSalary = array();
+        $maxSalary = array();
+        $colCode = array();
+        $author = array();
+        $date_posted = array();
+        $skills = array();
+        $requirements = array();
+        $personIDEncrypted = array();
+
+        if (mysqli_num_rows($result) > 0) {
+            $response = "Success";
+            while ($row_data = mysqli_fetch_assoc($result)) {
+                $careerID[] = $row_data['careerID'];
+                $jobTitle[] = $row_data['jobTitle'];
+                $companyName[] = $row_data['companyName'];
+                $jobDescript[] = $row_data['jobDescript'];
+                $jobQuali[] = $row_data['jobqualification'];
+                $companyLogo[] = base64_encode($row_data['companyLogo']);
+                $minSalary[] = $row_data['minSalary'];
+                $maxSalary[] = $row_data['colCode'];
+                $colCode[] = $row_data['colCode'];
+                $author[] = $row_data['author'];
+                $date_posted[] = $row_data['date_posted'];
+                // Pseudonymize the personID
+                $personIDEncrypted[] = $this->generatePseudonym($row_data['personID']);
+
+                //retrieve skills from the database
+                $skillQuery = 'SELECT * FROM `skill` WHERE careerID = "' . $row_data['careerID'] . '"';
+                $skillResult = mysqli_query($con, $skillQuery);
+                $skillNames = array();
+
+                if ($skillResult && mysqli_num_rows($skillResult) > 0) {
+                    while ($skill_data = mysqli_fetch_assoc($skillResult)) {
+                        $skillNames[] = $skill_data['skill'];
+                    }
+                }
+
+                $skills[] = $skillNames;
+
+                //retrieve requirements
+                $reqQuery = 'SELECT * FROM `requirement` WHERE careerID = "' . $row_data['careerID'] . '"';
+                $reqResult = mysqli_query($con, $reqQuery);
+                $requirement = array();
+
+                if ($reqResult && mysqli_num_rows($reqResult) > 0) {
+                    while ($req_data = mysqli_fetch_assoc($reqResult)) {
+                        $requirement[] = $req_data['requirement'];
+                    }
+                }
+                $requirements[] = $requirement;
+            }
+        } else $response = "Error";
+
+        //data to be sent
+        $data =  array(
+            'result' => $response,
+            'careerID' => $careerID,
+            'jobTitle' => $jobTitle,
+            'companyName' => $companyName,
+            'companyLogo' => $companyLogo,
+            'jobDescript' => $jobDescript,
+            'jobQuali' => $jobQuali,
+            'minSalary' => $minSalary,
+            'maxSalary' => $maxSalary,
+            'skills' => $skills,
+            'requirements' => $requirements,
+            'colCode' => $colCode,
+            'author' => $author,
+            'date_posted' => $date_posted,
+            'personID' => $personIDEncrypted,
+        );
+
+        echo json_encode($data); //return data as json
     }
 }
