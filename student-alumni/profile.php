31982c4b39da91222f1cfcbecab5b0d709cb8159
Isagani-lapira <lapiraisagani@gmail.com>
2023-07-29 16:16:43 +0800

    adding post functionality for profile tab

--- student-alumni/profile.php
+++ student-alumni/profile.php
@@ -85,6 +85,7 @@ function getAccDetails($con, $personID)
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
     <link rel="stylesheet" href="../style/profile.css" />
+    <link rel="stylesheet" href="../style/style.css" />
     <!-- <link rel="stylesheet" type="text/css" href="/style/mstyle.css"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
@@ -176,7 +177,6 @@ function getAccDetails($con, $personID)
             echo '<img src="' . $srcFormat . '" alt="Profile Icon" class="w-full h-96 object-cover" />';
         }
         ?>
-        <!-- <img src="../images/bganim.jpg" alt="Cover Photo" class="w-full h-96 object-cover"> -->
         <!-- Profile Photo (Intersecting with the Cover Photo) -->
         <div class="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 rounded-full border-4 border-accentBlue overflow-hidden w-56 h-56">
             <?php
@@ -457,6 +457,132 @@ function getAccDetails($con, $personID)
         </div>
     </div>
 
+    <!-- viewing of post -->
+    <div id="viewingPost" class="post modal fixed hidden inset-0 z-50 flex items-center justify-center p-3">
+        <div class="modal-container w-full h-full bg-white rounded-lg flex relative">
+            <span id="closePostModal" class="absolute top-0 right-0 text-center text-2xl cursor-pointer p-3 hover:scale-50 hover:font-bold">x</span>
+            <div id="containerSection" class="w-8/12 h-full ">
+
+                <div id="default-carousel" class="relative w-full h-full bg-black" data-carousel="slide">
+                    <!-- Carousel wrapper -->
+                    <div class="overflow-hidden rounded-lg h-full" id="carousel-wrapper"></div>
+                    <!-- Slider indicators -->
+                    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2" id="carousel-indicators">
+                    </div>
+                    <!-- Slider controls -->
+                    <button id="btnPrev" type="button" class="navigatorBtn absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none hover:bg-gray-500 hover:bg-opacity-20" data-carousel-prev>
+                        <span class="inline-flex items-center justify-center w-10 h-10 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                                <path fill="white" d="m4 10l9 9l1.4-1.5L7 10l7.4-7.5L13 1z" />
+                            </svg>
+                            <span class="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button id="btnNext" type="button" class="navigatorBtn text-white absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none hover:bg-gray-500 hover:bg-opacity-20">
+                        <span class="inline-flex items-center justify-center w-10 h-10">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="none" stroke="currentColor" stroke-width="2" d="m7 2l10 10L7 22" />
+                            </svg>
+                            <span class="sr-only">Next</span>
+                        </span>
+
+                    </button>
+                </div>
+
+            </div>
+
+            <!-- description -->
+            <div id="descriptionInfo" class="w-4/12 h-full p-2 border-l p-3 border-gray-400">
+                <div class="flex justify-start gap-2">
+                    <?php
+                    if ($profilepicture == "") {
+                        echo '<img id="profilePic" src="../assets/icons/person.png" alt="Profile Icon" class="rounded-full border-2 border-accent h-10 w-10" />';
+                    } else {
+                        $srcFormat = 'data:image/jpeg;base64,' . $profilepicture;
+                        echo '<img src="' . $srcFormat . '" alt="Profile Icon" class="rounded-full border-2 border-accent h-10 w-10" />';
+                    }
+                    ?>
+                    <div class="flex flex-col">
+                        <span id="postFullName" class=" text-greyish_black font-bold"></span>
+                        <span id="postUN" class=" text-gray-400 text-xs">username</span>
+                    </div>
+                </div>
+                <p id="postDescript" class=" text-greyish_black font-light text-sm">Description</p>
+
+                <div class="relative">
+                    <div class="flex justify-end gap-2 border-t border-gray-400 mt-5 items-center text-gray-400 text-sm py-2 px-3">
+                        <img src="../assets/icons/emptyheart.png" alt="">
+                        <span id="noOfLikes" class="cursor-pointer w-10 text-center"></span>
+                        <img src="../assets/icons/comment.png" alt="">
+                        <span id="noOfComment">0</span>
+                    </div>
+                    <div id="namesOfUser" class="absolute -bottom-2 right-0 bg-black opacity-25 text-gray-300 w-1/3 text-xs p-2 rounded-md hidden"></div>
+                </div>
+
+                <!-- comments -->
+                <div id="commentContainer" class=" h-3/4 p-2 overflow-auto">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="modal" class="modal fixed inset-0 z-50 h-full w-full flex items-center justify-center
+            text-grayish  top-0 left-0">
+        <div class="modal-container w-1/3 h-max bg-white rounded-lg p-3">
+            <div class="modal-header py-5 border-b border-gray-400">
+                <h1 class=" text-greyish_black text-2xl text-center font-bold">Create New Post</h1>
+            </div>
+            <div class="flex items-center mb-2 my-2">
+                <!-- set profile image -->
+                <?php
+                if ($profilepicture == "") {
+                    echo '<img src="../assets/icons/person.png" alt="Profile Icon" class="w-10 h-10 profile-icon rounded-full" />';
+                } else {
+                    $srcFormat = 'data:image/jpeg;base64,' . $profilepicture;
+                    echo '<img src="' . $srcFormat . '" alt="Profile Icon" class="w-10 h-10 profile-icon rounded-full" />';
+                }
+
+                ?>
+                <p class="text-black font-semibold text-sm pl-2">
+                    <?php
+                    echo $fullname;
+                    ?>
+                </p>
+            </div>
+
+            <div class="modal-body px-3 h-40">
+
+                <!-- body part -->
+                <div class="modal-descript relative w-full h-full rounded p-3">
+                    <div class="flex flex-col h-full border-gray-300">
+                        <textarea id="TxtAreaAnnouncement" class="rar outline-none w-full h-40" type="text" placeholder="Say something here..."></textarea>
+                    </div>
+                    <label for="fileGallery" class="cursor-pointer">
+                        <span id="galleryLogo" class="absolute bottom-1 left-1">
+                            <svg class="inline" width="22" height="22" viewBox="0 0 22 22" fill="green" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17 7C17 7.53043 16.7893 8.03914 16.4142 8.41421C16.0391 8.78929 15.5304 9 15 9C14.4696 9 13.9609 8.78929 13.5858 8.41421C13.2107 8.03914 13 7.53043 13 7C13 6.46957 13.2107 5.96086 13.5858 5.58579C13.9609 5.21071 14.4696 5 15 5C15.5304 5 16.0391 5.21071 16.4142 5.58579C16.7893 5.96086 17 6.46957 17 7Z" fill="#BCBCBC" />
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M10.943 0.25H11.057C13.366 0.25 15.175 0.25 
+                        16.587 0.44C18.031 0.634 19.171 1.04 20.066 1.934C20.961 2.829 21.366 3.969 21.56 5.414C21.75 6.825 21.75 8.634 21.75 10.943V11.031C21.75 12.94 21.75 14.502 21.646 15.774C21.542 17.054 21.329 18.121 20.851 19.009C20.641 19.4 20.381 19.751 20.066 20.066C19.171 20.961 18.031 21.366 16.586 21.56C15.175 21.75 13.366 21.75 11.057 
+                        21.75H10.943C8.634 21.75 6.825 21.75 5.413 21.56C3.969 21.366 2.829 20.96 1.934 20.066C1.141 19.273 0.731 18.286 0.514 17.06C0.299 15.857 0.26 14.36 0.252 12.502C0.25 12.029 0.25 11.529 0.25 11.001V10.943C0.25 8.634 0.25 6.825 0.44 5.413C0.634 3.969 1.04 2.829 1.934 1.934C2.829 1.039 3.969 0.634 5.414 0.44C6.825 0.25 8.634 0.25 10.943 0.25ZM5.613 1.926C4.335 2.098 3.564 2.426 2.995 2.995C2.425 3.565 2.098 4.335 1.926 5.614C1.752 6.914 1.75 8.622 1.75 11V11.844L2.751 10.967C3.1902 10.5828 3.75902 10.3799 4.34223 10.3994C4.92544 10.4189 5.47944 10.6593 5.892 11.072L10.182 15.362C10.5149 15.6948 10.9546 15.8996 11.4235 15.9402C11.8925 15.9808 12.3608 15.8547 12.746 15.584L13.044 15.374C13.5997 14.9835 14.2714 14.7932 14.9493 14.834C15.6273 14.8749 16.2713 15.1446 16.776 15.599L19.606 18.146C19.892 17.548 20.061 16.762 20.151 15.653C20.249 14.448 20.25 12.946 20.25 11C20.25 8.622 20.248 6.914 20.074 5.614C19.902 4.335 19.574 3.564 19.005 2.994C18.435 2.425 17.665 2.098 16.386 1.926C15.086 1.752 13.378 1.75 11 1.75C8.622 1.75 6.913 1.752 5.613 1.926Z" fill="#BCBCBC" />
+                            </svg>
+                            Add Image
+                        </span>
+                    </label>
+                    <input id="fileGallery" type="file" class="hidden" />
+                </div>
+
+            </div>
+            <div id="imgContPost" class="hidden flex overflow-x-scroll w-full border-t border-gray-300"></div>
+            <p class="text-sm text-red-400 hidden" id="errorMsg">Sorry we only allow images that has file extension of
+                jpg,jpeg,png</p>
+            <!-- Footer -->
+            <div class="modal-footer flex items-end flex-row-reverse px-3">
+                <button id="postBtn" class="bg-accent py-2 rounded px-5 text-white font-semibold ms-3 hover:bg-darkAccent">Post</button>
+                <button class="cancel py-2 rounded px-5 text-grayish border border-slate-400 hover:bg-slate-400 hover:text-white">Cancel</button>
+            </div>
+        </div>
+    </div>
 
     <script src="../student-alumni/js/profile.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
