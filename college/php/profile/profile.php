5570b62008c5486e8bd5d7516fbd0e087107e045
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-08-14 21:47:21 +0800

    Add php data to profile

--- college/php/profile/profile.php
+++ college/php/profile/profile.php
@@ -1,40 +1,61 @@
+<?php
+session_start();
+require_once('../model/Alumni.php');
+require_once("../../../PHP_process/connection.php");
+
+
+$alumniData = [];
+// get the id of the user from session id
+if (isset($_SESSION['personID'])) {
+    $alumni = new Alumni($mysql_con);
+    // todo refactor later
+    $alumniData = json_decode($alumni->getFullAlumniDetailById($_SESSION['personID'], false), true)['result'];
+} else {
+    echo "<h1>" . "No person ID" .  "</h1>";
+    die();
+}
+
+?>
 <section>
 
     <!-- profile content -->
     <div class="p-3 rounded-md bg-accent flex items-center my-3">
         <img class="h-36 w-36 rounded-full border-2 border-white" src="/images/Mr.Jayson.png" alt="">
         <div class="ms-6">
-            <p class="text-lg text-white font-bold">Jayson Batoon</p>
+            <p class="text-lg text-white font-bold">
+                <?= $alumniData['fname'] . ' ' . $alumniData['lname'] ?>
+            </p>
             <p class="text-blue-300 hover:cursor-pointer hover:text-blue-500">Edit Profile</p>
         </div>
     </div>
     <div class="flex text-grayish">
+
         <!-- about section -->
         <section class="w-1/4 text-sm m-2 p-4 mr-5 border">
             <p class="font-bold text-accent">About</p>
             <div class="flex mt-3 justify-start">
                 <img src="/assets/icons/person.png" alt="">
-                <span class="px-2">Male</span>
+                <span class="px-2"><?= $alumniData['gender'] ?></span>
             </div>
 
             <div class="flex mt-3">
                 <img src="/assets/icons/cake.png" alt="">
-                <span class="px-2">Born June 26, 1980</span>
+                <span class="px-2">Born <?= $alumniData['bday'] ?></span>
             </div>
 
             <div class="flex mt-3">
                 <img class="ps-1 messageIcon" src="/assets/icons/Location.png" alt="">
-                <span class="px-3">32 Sta. Monica Bulakan Bulacan</span>
+                <span class="px-3"><?= $alumniData['address'] ?></span>
             </div>
 
             <div class="flex mt-3">
                 <img class="ps-1 " src="/assets/icons/Message.png" alt="">
-                <span class="px-4">jaysonbatoon@gmail.com</span>
+                <span class="px-4"><?= $alumniData['bulsu_email'] ?></span>
             </div>
 
             <div class="flex mt-3">
                 <img class="ps-1" src="/assets/icons/Call.png" alt="">
-                <span class="px-4">09323887301</span>
+                <span class="px-4"><?= $alumniData['contactNo'] ?></span>
             </div>
         </section>
 
@@ -48,7 +69,9 @@
                     <div class="flex justify-start items-center">
                         <div class="flex items-center">
                             <img class="h-12 border-2 border-accent rounded-full" src="/images/Mr.Jayson.png" alt="">
-                            <p class="text-start px-3 text-sm font-semibold">Jayson Batoon</p>
+                            <p class="text-start px-3 text-sm font-semibold">
+
+                            </p>
                         </div>
                         <img class="ml-auto" src="/assets/more_horiz.png" alt="">
                     </div>
