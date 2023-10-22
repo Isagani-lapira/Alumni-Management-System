53d4236d8654d65d6bbdbf43ecd9b917b85379c9
Isagani Lapira <lapiraisagani@gmail.com>
2023-10-22 19:03:29 +0800

    Update connection.php

--- PHP_process/connection.php
+++ PHP_process/connection.php
@@ -1,12 +1,11 @@
 <?php
-    $host = 'localhost';
-    $username = 'root';
-    $password = "";
-    $database = "alumni_management_system";
+$host = 'localhost';
+$username = 'u673355866.eliamarg';
+$password = "Ga#1%L*DHFdR5&";
+$database = "u673355866_eliamarg";
 
-    $mysql_con = new mysqli($host,$username,$password,$database);
+$mysql_con = new mysqli($host, $username, $password, $database);
 
-    if($mysql_con->connect_error){
-        die('Connection Failed: '.$mysql_con->connect_error);
-    }
-?>
+if ($mysql_con->connect_error) {
+    die('Connection Failed: ' . $mysql_con->connect_error);
+}
