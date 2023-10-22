22d0d1ab9c7d39e0129ac4e9d669e8f9b35fd8de
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-10-23 07:05:12 +0800

    add: design to the password-change

--- password-recovery/password-changed.php
+++ password-recovery/password-changed.php
@@ -1,7 +1,8 @@
-<?php require_once "controller.php"; ?>
+<?php require_once "controller.php";
+?>
 <?php
 if ($_SESSION['info'] == false) {
-    header('Location: login-user.php');
+    header('Location: ../student-alumni/login.php');
 }
 ?>
 <!DOCTYPE html>
@@ -10,32 +11,36 @@ if ($_SESSION['info'] == false) {
 <head>
     <meta charset="UTF-8">
     <title>Login Form</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css"> -->
+    <!--   Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <!-- Icons -->
+    <!-- Font-awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="../css/main.css">
+
+
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 offset-md-4 form login-form">
-                <?php
-                if (isset($_SESSION['info'])) {
-                ?>
-                    <div class="alert alert-success text-center">
-                        <?php echo $_SESSION['info']; ?>
-                    </div>
-                <?php
-                }
-                ?>
-                <form action="login-user.php" method="POST">
-                    <div class="form-group">
-                        <input class="form-control button" type="submit" name="login-now" value="Login Now">
+    <div class="daisy-hero min-h-screen bg-base-200">
+        <div class="daisy-hero-content flex-col ">
+            <div class="daisy-card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+                <form class="daisy-card-body">
+                    <h1 class="font-bold text-xl text-center">Successfully Reset Your Password</h1>
+                    <p class="py-6">You may now login with your freshly made password!</p>
+                    <div class="daisy-form-control mt-4">
+                        <a href="../student-alumni/login.php" class="daisy-btn daisy-btn-neutral bg-accent">Return to Login</a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-
 </body>
 
 </html>
--- password-recovery/reset-code.php
+++ password-recovery/reset-code.php
@@ -17,6 +17,17 @@ if ($email == false) {
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css"> -->
 
+    <!--   Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <!-- Icons -->
+    <!-- Font-awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+
+
     <!-- Style Sheeet -->
     <link rel="stylesheet" href="../css/main.css">
 </head>
