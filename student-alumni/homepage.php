9995f4c139470193f698fbfd420c8bab32db1474
Isagani Lapira <lapiraisagani@gmail.com>
2023-10-23 15:58:40 +0800

    Adding job repository loading indicator and changing the maximum of retrieval to 10

--- student-alumni/homepage.php
+++ student-alumni/homepage.php
@@ -238,16 +238,20 @@ function getAccDetails($con, $personID)
             </div>
 
             <!-- Verification Job Post -->
-            <div id="target-div-job" class="div-btn flex items-center hover:bg-gray-100 rounded-md h-10 p-2 mt-1">
+            <?php
+            if ($user_type == "alumni") {
+              echo '
+              <div id="target-div-job" class="div-btn flex items-center hover:bg-gray-100 rounded-md h-10 p-2 mt-1">
               <button id="verif-btn" onclick="toggleColorJob(), toggleJobPost()">
                 <svg class="inline fa" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                   <path fill="currentColor" d="m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z" />
                 </svg>
                 <span class="ps-3 text-sm text-greyish_black font-medium">Job Repository</span>
               </button>
-            </div>
+            </div>';
+            }
+            ?>
 
-            <!-- Yearbook -->
             <!-- show only yearbook for alumni -->
             <?php
             if ($user_type == "alumni") {
@@ -337,7 +341,10 @@ function getAccDetails($con, $personID)
               </div>
 
               <!-- Job Post Feed -->
-              <div id="jobRepo" class="hidden h-max grid grid-cols-3 gap-2 overflow-y-auto no-scrollbar py-3"></div>
+              <div id="jobRepo" class="hidden h-max grid grid-cols-3 gap-2 overflow-y-auto no-scrollbar py-3">
+                <p id="loadingDataJobRepo" class="text-gray-400 text-center">Loading repository</p>
+                <p id="nojobrepo" class="text-gray-400 text-center">No data to be display</p>
+              </div>
 
             </div>
 
