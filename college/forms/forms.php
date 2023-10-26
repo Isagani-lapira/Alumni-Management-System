1284e6298f1eb2563b6a45ae8054a2205c25e4b2
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-10-26 09:03:22 +0800

    dump: tracer form

--- college/forms/forms.php
+++ college/forms/forms.php
@@ -1,28 +1,133 @@
 <section class=" mx-auto   p-5 lg:mx-8">
-    <h1 class="text-2xl font-bold ">Alumni Tracer Form</h1>
-    <p class="text-gray-400 font-semibold">See the relevant information that are gathered</p>
+    <h1 class="text-xl font-extrabold">Alumni Tracer Form</h1>
+    <p class="text-grayish">See the relevant information that are gathered</p>
 
-
-    <hr class="my-10">
-
-    <!-- Pie Chart -->
-
-    <h3 class="text-subheading">Employee Status</h3>
-
-
-    <!-- TODO add chart here -->
-    <div class="chart-box w-72 relative">
-        <canvas id="empStatusPieChart"></canvas>
+    <div class="flex gap-2 justify-end mb-2">
+        <button id="tracerbtn" class="text-gray-400 hover:text-gray-500">Tracer form</button>
+        <button id="deployTracerBtn" class="px-3 py-2 bg-accent hover:bg-darkAccent text-white rounded-md font-bold">Deploy Tracer</button>
     </div>
 
-    <!-- Chart.js  -->
-    <h3>Annual Salary</h3>
-    <div class="chart-box w-1/2 h-64 relative">
-        <canvas id="annualSalCanvas"></canvas>
+    <div id="formReport" class="border border-t-grayish h-full overflow-y-auto">
+        <div class="flex gap-2 justify-evenly">
+            <div class="h-2/5 w-1/2 p-5 flex flex-col">
+                <h1 class="text-lg font-extrabold">Completion Chart</h1>
+                <canvas class="w-full h-full" id="completionChart"></canvas>
+            </div>
+
+            <div class="h-2/5 w-1/2 p-5 flex flex-col">
+                <h1 class="text-lg font-extrabold px-5">College Alumni Chart</h1>
+                <canvas class="w-full h-5/6" id="respondentPerCol"></canvas>
+            </div>
+        </div>
+
+        <div class="center-shadow rounded-lg p-3 m-2">
+            <h3 class="font-semibold text-lg text-greyish_black">GET THE GRAPH OF A SPECIFIC QUESTION</h3>
+
+            <div class="flex mt-2 items-center">
+                <div class=" w-3/4 flex">
+                    <!-- category -->
+                    <div class="text-sm text-gray-500 flex flex-col w-1/2">
+                        <label for="categorySelection" class="px-2">CATEGORY</label>
+                        <select id="categorySelection" class="border border-gray-400 rounded-lg p-2 m-2">
+                            <option value="" selected>--Your Choice--</option>
+                        </select>
+                    </div>
+                    <!-- question -->
+                    <div class="text-sm text-gray-500 flex flex-col w-1/2">
+                        <label for="questionSelection" class="px-2">QUESTION</label>
+                        <select id="questionSelection" class="border border-gray-400 rounded-lg p-2 m-2">
+                            <option value="" selected>--Your Choice--</option>
+                        </select>
+                    </div>
+
+                    <!-- graph type -->
+                    <div class="text-sm text-gray-500 flex flex-col w-1/2">
+                        <label for="typeChartSelection" class="px-2">GRAPH/CHART</label>
+                        <select id="typeChartSelection" class="border border-gray-400 rounded-lg p-2 m-2">
+                            <option value="" selected>--Your Choice--</option>
+                            <option value="bar">Bar Type</option>
+                            <option value="bubble">Bubble Type</option>
+                            <option value="doughnut">Doughnut Type</option>
+                            <option value="pie">Pie Type</option>
+                            <option value="line">Line Type</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                <div class="w-1/2 flex items-center justify-end">
+                    <button id="displayChart" class="px-4 py-2 rounded-lg off">Display</button>
+                </div>
+
+            </div>
+
+            <p class="text-sm italic text-gray-500">Note: Select a category first</p>
+        </div>
+
+        <div class="center-shadow rounded-lg p-3 m-2 h-full flex justify-center">
+            <canvas id="chartPerQuestion"></canvas>
+        </div>
     </div>
+
+    <!-- repository -->
+    <div id="tracerRepo" class="h-full border border-t-grayish p-5 w-full hidden">
+        <div class="flex justify-end my-2">
+            <div id="previewFormBtn" class="w-max cursor-pointer hover:font-bold hover:text-accent flex items-center gap-2">
+                <iconify-icon icon="icon-park-outline:preview-open" width="24" height="24"></iconify-icon>
+                <span>Preview</span>
+            </div>
+        </div>
+
+        <div id="TracerWrapper" class="flex w-full h-full mx-auto p-2 gap-2">
+
+            <div id="categoryWrapper" class="flex flex-col gap-2 w-1/3 p-1  h-full"></div>
+            <!-- question set -->
+            <div class="flex-1 h-full center-shadow p-3 relative">
+                <input id="categoryName" class="w-full py-2 text-xl text-greyish_black border-b border-gray-400 font-semibold mb-3" disabled />
+                <span id="btnSaveChanges" class="absolute top-5 right-2 text-gray-400 text-xs flex items-center gap-2 hidden" id="savedChanges">
+                    <iconify-icon icon="dashicons:saved" style="color: #afafaf;" width="20" height="20"></iconify-icon>
+                </span>
+                <div id="questionSetContainer" class="overflow-y-auto flex flex-col gap-2 py-2"></div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- preview -->
+    <div id="previewForm" class="p-3 flex flex-col border-t border-gray-500 h-full overflow-y-auto hidden">
+        <div>
+            <div id="backFromPreviewBtn" class="flex items-center gap-3 w-max">
+                <iconify-icon icon="fluent-mdl2:back" class="text-gray-800 cursor-pointer" width="24" height="24"></iconify-icon>
+                <span class="font-semibold">Back</span>
+            </div>
+
+        </div>
+
+        <h2 class="font-bold text-center text-lg">Alumni Tracer Form</h2>
+        <div id="previewContainer" class="flex flex-col p-5 gap-3 border border-gray-300 rounded-md">
+            <h3 id="categoryNamePrev" class="font-bold text-lg text-start"></h3>
+        </div>
+
+        <div class="mt-2">
+            <button id="previousPreviewBtn" class="hidden text-gray-500 hover:text-lg">Previous</button>
+            <button id="nextPreviewBtn" class="mt-2 w-max py-2 px-5 rounded-md bg-accent text-white hover:bg-darkAccent">Next</button>
+        </div>
+
+    </div>
+
+
+
 
 
 </section>
+
+
+
+<script type="module" src="./forms/tracer.js"></script>
+<script type="module" src="./forms/tracerchart.js"></script>
+
+
+
 
 <script>
     $(document).ready(function() {
---
+++ college/forms/tracerchart.js
@@ -0,0 +1,283 @@
+$(document).ready(function () {
+
+    // get the total completion chart
+    $('#formLi').on('click', function () {
+        $('#forms-tab').removeClass('hidden')
+        retrieveCompletionData()
+        retrieveCollegeParticipation()
+        addCategorySelection()
+    })
+
+    const completionChart = $('#completionChart')[0].getContext('2d');
+    const completionChartObj = new Chart(completionChart, {
+        type: 'pie',
+        data: {
+            labels: ['Completed', 'Waiting'],
+            datasets: [{
+                label: '# of Votes',
+                data: [35, 500],
+                borderWidth: 1,
+                backgroundColor: [
+                    '#CA472F', '#8DDDD0'
+                ]
+            }]
+        },
+        options: {
+            responsive: false,
+        }
+    });
+
+
+    const collegeChart = $('#respondentPerCol')[0].getContext('2d');
+    const chartData = {
+        type: 'bar',
+        data: {
+            labels: [],
+            datasets: [{
+                label: '# of College Alumni already finished answering',
+                data: [],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true, // Make the chart responsive
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 1, // Specify the step size for Y-axis values
+                    }
+                }
+
+            }
+        }
+    };
+
+    const myChart = new Chart(collegeChart, chartData);
+    // Function to update the chart with data
+    function updateChart(labels, data) {
+        myChart.data.labels = labels;
+        myChart.data.datasets[0].data = data;
+        myChart.update();
+    }
+
+    function retrieveCollegeParticipation() {
+        const action = "countDonePerCollege";
+        const formData = new FormData();
+        formData.append('action', action)
+
+        $.ajax({
+            url: '../PHP_process/answer.php',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: response => {
+                // collect data retrieve
+                const labels = [];
+                const countData = [];
+                response.forEach(data => {
+                    const collegeName = data.colCode;
+                    const dataCount = data.alumniCountFinished;
+
+                    labels.push(collegeName);
+                    countData.push(dataCount);
+                });
+
+                updateChart(labels, countData) //update chart using the data retrieved
+            },
+            error: error => { console.log(error) }
+        })
+    }
+
+    function retrieveCompletionData() {
+        const action = "completionStatus"
+        const formData = new FormData()
+        formData.append('action', action)
+
+        $.ajax({
+            url: '../PHP_process/answer.php',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: response => {
+                const completedCount = response.completed;
+                const waitingCount = response.waiting;
+
+                // update the chart based on the retrieve data
+                completionChartObj.data.datasets[0].data = [completedCount, waitingCount];
+                completionChartObj.update();
+            }
+        })
+    }
+
+
+    // add category to the selection
+    function addCategorySelection() {
+        const action = "retrievedCategory"
+        const formData = new FormData();
+        formData.append('action', action)
+        $.ajax({
+            url: '../PHP_process/graduatetracer.php',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: response => {
+                if (response.result == 'Success') {
+                    // add the category to selection of category
+                    const length = response.categoryID.length;
+
+                    for (let i = 0; i < length; i++) {
+                        const categoryID = response.categoryID[i];
+                        const categoryName = response.categoryName[i];
+
+                        const option = $('<option>').text(categoryName).val(categoryID);
+                        $('#categorySelection').append(option)
+                    }
+                }
+            },
+            error: error => { console.log(error) }
+        })
+    }
+
+
+    // add question
+    $('#categorySelection').on('change', function () {
+        const categoryVal = $(this).val()
+        const action = 'readQuestions';
+        const formData = new FormData();
+        formData.append('action', action);
+        formData.append('categoryID', categoryVal);
+
+        // retrieve all question that is in category
+        $.ajax({
+            url: '../PHP_process/graduatetracer.php',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'JSON',
+            success: response => {
+                $('#questionSelection').find(':not(:first-child)').remove()
+                if (response.response = 'Success') {
+                    const dataSet = response.dataSet[0].questionSet
+                    const choicesData = []
+                    dataSet.forEach(data => {
+                        let questionID = data.questionID
+                        let questionTxt = data.questionTxt
+                        let inputType = data.inputType
+
+                        // only question with choices
+                        if (inputType !== 'Input') {
+                            let option = $('<option>').text(questionTxt).val(questionID)
+                            $('#questionSelection').append(option)
+
+                        }
+
+                    })
+                }
+            },
+            error: error => { console.log(error) },
+        })
+    })
+
+    $('#questionSelection').on('change', function () {
+        let value = $(this).val();
+
+        $('#displayChart')
+            .removeClass('off')
+            .addClass('on')
+            .on('click', function () {
+                displayChartForQuestion(value)
+            })
+    })
+
+
+    function displayChartForQuestion(questionID) {
+        const action = "getQuestionChoices"
+        const formData = new FormData();
+        formData.append('action', action);
+        formData.append('questionID', questionID);
+
+        let labels = []
+        let counts = []
+        // get all the choices in  a particular question
+        $.ajax({
+            url: '../PHP_process/graduatetracer.php',
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'JSON',
+            success: response => {
+                const length = response.length
+                for (let i = 0; i < length; i++) {
+                    // choice and its corresponding answers chosen
+                    const choiceText = response[i].choiceText
+                    const count = response[i].count
+                    labels.push(choiceText)
+                    counts.push(count)
+                }
+
+                typeChartSelection = $('#typeChartSelection').val()
+
+                if (typeChartSelection != "")
+                    updateQuestionChartData(labels, counts, typeChartSelection)
+                else updateQuestionChartData(labels, counts)
+                labels = []
+                counts = []
+            },
+        })
+    }
+
+    const colors = ['#E6B0AA', '#D7BDE2', '#A9CCE3', '#A3E4D7',
+        '#A9DFBF', '#F9E79F', '#F5CBA7', '#D5DBDB', '#D5DBDB', '#AEB6BF', '#8D6E63',
+        '#00BCD4', '#78909C', '#C0CA33', '#117864', '#212F3C'];
+
+    function updateQuestionChartData(labels, data, typeChartSelection = "pie") {
+        // changing the chart based on reference of the user
+        questionChartObj.data.labels = labels; //choices as labels
+        questionChartObj.data.datasets[0].data = data; //count per choices
+        questionChartObj.config.type = typeChartSelection; //type of chart
+        const questionText = $('#questionSelection option:selected').text() //label for chart the selected option text
+        questionChartObj.data.datasets[0].label = "# of Votes in " + questionText
+        const backgroundColors = [];
+        // background colors
+        for (let i = 0; i < data.length; i++) {
+            backgroundColors.push(colors[i]);
+        }
+
+        if (typeChartSelection !== "pie") {
+            console.log('pumasok')
+            questionChartObj.options.scales = {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 1, // Specify the step size for Y-axis values
+                    }
+                }
+            }
+        }
+        questionChartObj.data.datasets[0].backgroundColor = backgroundColors
+        questionChartObj.update();
+    }
+
+    const questionChart = $('#chartPerQuestion')[0].getContext('2d');
+    const questionChartObj = new Chart(questionChart, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: '# of Votes',
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            responsive: true,
+        }
+    });
+})
