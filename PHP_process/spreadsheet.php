4553515e7f0c90cb8ee15fdefc8b35bf3a528635
Isagani Lapira <lapiraisagani@gmail.com>
2023-10-31 18:56:51 +0800

    Fetching and saving graduate tracer data in spreadsheet file

--- PHP_process/spreadsheet.php
+++ PHP_process/spreadsheet.php
@@ -16,7 +16,7 @@ function getData($con, $worksheet)
         SELECT td.tracer_deployID
         FROM tracer_deployment td
         ORDER BY td.timstamp
-        LIMIT 1)";
+        LIMIT 1) AND a.status = 'done'";
 
     $stmt = mysqli_prepare($con, $query);
     $stmt->execute();
@@ -25,27 +25,29 @@ function getData($con, $worksheet)
     if ($result) {
         // Get question texts using the getQuestions function
         $questionTexts = getQuestions($con);
-
-        // Set column names in the spreadsheet
         $column = 'A';
+
+        $worksheet->setTitle('General Questions');
+        // add every question in row 1
         foreach ($questionTexts as $questionText) {
             $worksheet->setCellValue($column . '1', $questionText);
-            $worksheet->getStyle($column . '1')->getFont()->setBold(true);
-            $worksheet->getColumnDimension($column)->setAutoSize(true); // Adjust column width
+            $worksheet->getStyle($column . '1')->getFont()->setBold(true); //make the font bold
+            $worksheet->getColumnDimension($column)->setAutoSize(true); //adjust the cell depending on the text
             $column++;
         }
 
-        $row = 2; // Start from the second row
+        $row = 2; //start from second row
+        //add every answer on every question
         while ($data = $result->fetch_assoc()) {
             $answerID = $data['answerID'];
 
-            // get all the question
+            // get all questions
             $queryQuestion = "SELECT tq.`questionID`, tq.`question_text`
             FROM `tracer_question` tq
             INNER JOIN `question_category` qc ON tq.`categoryID` = qc.`categoryID`
-            WHERE qc.`status` = 'available' AND tq.`isSectionQuestion` = 0
+            WHERE qc.`status` = 'available' AND tq.`isSectionQuestion` = 0 AND
+            tq.`status` = 'available'
             ORDER BY qc.`sequence` ASC, tq.`sequence` ASC";
-
             $stmtQuestion = mysqli_prepare($con, $queryQuestion);
             $stmtQuestion->execute();
             $questionResult = $stmtQuestion->get_result();
@@ -57,24 +59,28 @@ function getData($con, $worksheet)
 
                     // get the  answer for every question
                     $queryAnswer = "SELECT 
-                    CASE
-                        WHEN qc.choice_text IS NOT NULL THEN qc.choice_text
-                        ELSE ad.answer_txt
-                    END AS answer_val
-                    FROM answer_data ad
-                    LEFT JOIN questionnaire_choice qc ON ad.choiceID = qc.choiceID
-                    WHERE ad.answerID = ?
-                    AND ad.questionID = ?";
+                                CASE
+                                    WHEN qc.choice_text IS NOT NULL THEN qc.choice_text
+                                    ELSE ad.answer_txt
+                                END AS answer_val
+                                FROM answer_data ad
+                                LEFT JOIN questionnaire_choice qc ON ad.choiceID = qc.choiceID
+                                WHERE ad.answerID = ?
+                                AND ad.questionID = ?";
 
                     $stmtAnswer = mysqli_prepare($con, $queryAnswer);
                     $stmtAnswer->bind_param('ss', $answerID, $questionID);
                     $stmtAnswer->execute();
                     $resultAnswer = $stmtAnswer->get_result();
+                    $rowQuery = mysqli_num_rows($resultAnswer);
 
-                    if ($resultAnswer) {
-                        $questionAnswer = $resultAnswer->fetch_assoc()['answer_val'];
-                        $worksheet->setCellValue($column . $row, $questionAnswer);
-                        $column++;
+                    if ($rowQuery > 0) {
+                        $answer = $resultAnswer->fetch_assoc()['answer_val'];
+
+                        if ($answer != null) {
+                            $worksheet->setCellValue($column . $row, $answer);
+                            $column++;
+                        }
                     }
                 }
                 $row++; // Move to the next row for the next set of answers
@@ -83,6 +89,7 @@ function getData($con, $worksheet)
     }
 }
 
+// function to retrieve all the question
 function getQuestions($con)
 {
     $questionTexts = array();
@@ -90,7 +97,7 @@ function getQuestions($con)
     $queryQuestion = "SELECT tq.`questionID`, tq.`question_text`
     FROM `tracer_question` tq
     INNER JOIN `question_category` qc ON tq.`categoryID` = qc.`categoryID`
-    WHERE qc.`status` = 'available' AND tq.`isSectionQuestion` = 0
+    WHERE qc.`status` = 'available' AND tq.`isSectionQuestion` = 0 AND tq.`status` = 'available'
     ORDER BY qc.`sequence` ASC, tq.`sequence` ASC";
 
     $stmtQuestion = mysqli_prepare($con, $queryQuestion);
