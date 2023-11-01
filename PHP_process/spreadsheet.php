7f4ac60600152a9d6ca8860afd931d75987c0a8d
Isagani Lapira <lapiraisagani@gmail.com>
2023-11-01 09:29:12 +0800

    Fetching and section data in spreadsheet file as another worksheet

--- PHP_process/spreadsheet.php
+++ PHP_process/spreadsheet.php
@@ -113,8 +113,147 @@ function getQuestions($con)
     return $questionTexts;
 }
 
-getData($mysql_con, $activeWorksheet);
+function createSectionSheet($spreadsheet, $con)
+{
+    $categories = getSectionWithCategory($con); //retrieve all the section question
 
+    foreach ($categories as $categoryName) {
+        addDataWorkSheet($categoryName, $spreadsheet, $con);
+    }
+}
+
+function addDataWorkSheet($categoryName, $spreadsheet, $con)
+{
+    // add worksheet
+    $newWorkSheet = $spreadsheet->createSheet();
+    $newWorkSheet->setTitle($categoryName);
+
+    // fetch all the section question for that category
+    $queryCatQuestion = "SELECT DISTINCT tq.`question_text`
+    FROM `tracer_question` tq
+    JOIN `question_category` cat ON tq.`categoryID` = cat.`categoryID`
+    WHERE cat.`category_name` = ?
+    AND cat.status= ? AND tq.status = ?
+    AND tq.`isSectionQuestion` = ? ";
+
+    $stmt = mysqli_prepare($con, $queryCatQuestion);
+    if ($stmt) {
+        $STATUS = 'available';
+        $ISSECTIONQUESTION = 1;
+        $stmt->bind_param('sssd', $categoryName, $STATUS, $STATUS, $ISSECTIONQUESTION);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $ROWHEADER = 1; //header row
+        $column = 'A';
+        $NAMEQUESTION = "Name";
+        $newWorkSheet->setCellValue($column . $ROWHEADER, $NAMEQUESTION);
+        $newWorkSheet->getStyle($column . $ROWHEADER)->getFont()->setBold(true);
+        $newWorkSheet->getColumnDimension($column)->setAutoSize(true);
+        $column++; //To start on B
+
+        $questions = array();
+        // add the question as header in excel
+        while ($data = $result->fetch_assoc()) {
+            $question = $data['question_text'];
+            $newWorkSheet->setCellValue($column . $ROWHEADER, $question); //header
+            $newWorkSheet->getStyle($column . $ROWHEADER)->getFont()->setBold(true);
+            $newWorkSheet->getColumnDimension($column)->setAutoSize(true);
+            $column++;
+            $questions[] = $question;
+        }
+        $stmt->close();
+
+        // retrieve all the answer
+        $queryAnswer = "SELECT a.answerID, a.personID, CONCAT(p.lname,p.fname) as 'fullname'
+        FROM answer a
+        JOIN person p ON a.personID = p.personID
+        WHERE a.tracer_deployID = ? AND a.status = ? AND a.status = 'done'";
+        $stmtAnswer = mysqli_prepare($con, $queryAnswer);
+
+        $STATUS = 'done';
+        $tracer_deployID = 'e6cb4908e273c7867a3e88ee55278';
+        if ($stmtAnswer) {
+            $stmtAnswer->bind_param('ss', $tracer_deployID, $STATUS);
+            $stmtAnswer->execute();
+            $resultAnswer = $stmtAnswer->get_result();
+            $row = mysqli_num_rows($resultAnswer);
+
+            if ($resultAnswer && $row > 0) {
+                $columnAnswer = 'A';
+                $rowval = 2;
+                $newWorkSheet->setCellValue($columnAnswer . $rowval, 'isagani');
+                // echo $resultAnswer->fetch_assoc();
+                while ($data = $resultAnswer->fetch_assoc()) {
+                    $fullname = $data['fullname'];
+                    $answerID = $data['answerID'];
+
+                    $newWorkSheet->setCellValue($columnAnswer . $rowval, $fullname);
+                    // get other answer per question
+                    foreach ($questions as $question) {
+                        $columnAnswer++;
+                        getSectionAnswerPerQ($answerID, $question, $newWorkSheet, $rowval, $columnAnswer, $con);
+                    }
+                    $rowval++;
+                    $columnAnswer = 'A';
+                }
+            }
+        }
+    }
+}
+
+function getSectionAnswerPerQ($answerID, $questionText, $spreadSheet, $rowval, $columnAnswer, $con)
+{
+    $query = "SELECT
+            CASE
+                WHEN qc.`choice_text` IS NOT NULL THEN qc.`choice_text`
+                ELSE ad.`answer_txt`
+            END AS `selected_text`
+            FROM `answer_data` AS ad
+            INNER JOIN `tracer_question` AS tq ON ad.`questionID` = tq.`questionID`
+            LEFT JOIN `questionnaire_choice` AS qc ON ad.`choiceID` = qc.`choiceID`
+            WHERE ad.`answerID` = ?
+            AND tq.`question_text` = ? ";
+
+    $stmtAnswerPerQ = mysqli_prepare($con, $query);
+
+    if ($stmtAnswerPerQ) {
+        $stmtAnswerPerQ->bind_param('ss', $answerID, $questionText);
+        $stmtAnswerPerQ->execute();
+        $result = $stmtAnswerPerQ->get_result();
+        $row = mysqli_num_rows($result);
+
+        $data = 'N/A';
+        if ($result && $row > 0) $data = $result->fetch_assoc()['selected_text'];
+
+        $spreadSheet->setCellValue($columnAnswer . $rowval, $data);
+    }
+}
+
+function getSectionWithCategory($con)
+{
+    $categoryName = array();
+    $query = "SELECT DISTINCT qc.`category_name`
+    FROM `question_category` qc
+    JOIN `tracer_question` tq ON qc.`categoryID` = tq.`categoryID`
+    WHERE tq.`isSectionQuestion` = 1
+    ORDER BY qc.`sequence` ASC";
+
+    $stmtQuestion = mysqli_prepare($con, $query);
+    $stmtQuestion->execute();
+    $categoryResult = $stmtQuestion->get_result();
+
+    if ($categoryResult) {
+        while ($categoryData = $categoryResult->fetch_assoc()) {
+            $categoryName[] = $categoryData['category_name'];
+        }
+    }
+
+    return $categoryName;
+}
+
+getData($mysql_con, $activeWorksheet);
+createSectionSheet($spreadsheet, $mysql_con);
 $writer = new Xlsx($spreadsheet);
 
 // Set response headers to indicate a downloadable Excel file
