fc3126b80f9854e44778a4c625ab5885dffe1523
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-11-02 09:05:55 +0800

    add: post on make-post

--- college/make-post/apiPosts.php
+++ college/make-post/apiPosts.php
@@ -2,6 +2,7 @@
 session_start();
 
 require "../php/connection.php";
+require "../php/logging.php";
 require_once "../php/checkLogin.php";
 // Path: college/make-post/apiPosts.php
 
@@ -11,18 +12,103 @@ require_once "../php/checkLogin.php";
 
 header("Content-Type: application/json");
 
+// check if it is a get request
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET["all"])) {
+        get_all_alumni();
+    } else if (isset($_GET["id"])) {
+        apiGet($_GET["id"]);
+    } else if (isset($_GET["edit"])) {
+        apiPostEdit($_GET["edit"]);
+    } else if (isset($_GET["delete"])) {
+        apiPostDelete($_GET["delete"]);
+    } else {
+        get_all_alumni();
+    }
+} else if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-if (isset($_GET["all"])) {
-    get_all_alumni();
-} else if (isset($_GET["id"])) {
-    apiGet($_GET["id"]);
-} else if (isset($_GET["edit"])) {
-    apiPostEdit($_GET["edit"]);
-} else if (isset($_GET["delete"])) {
-    apiPostDelete($_GET["delete"]);
-} else {
-    get_all_alumni();
+
+    // check if there is action 
+    if (isset($_POST["action"])) {
+        if ($_POST["action"] == "createPost") {
+
+
+            $username = $_SESSION["username"];
+            $colCode = $_SESSION["colCode"];
+            $adminID = $_SESSION["adminID"];
+
+            $caption = $_POST["description"];
+            $img = (isset($_FILES['files'])) ? $_FILES['files'] : null;
+
+            $date = date('y/m/d');
+            $random = rand(0, 4000);
+            $postID = uniqid() . '-' . $random;
+
+
+            function insertImgPost($postID, $img, $mysql_con)
+            {
+                $random = rand(0, 100);
+                $imageID = $postID . '-' . uniqid() . '-' . $random;
+
+                //add image to the database
+                $query = "INSERT INTO `post_images`(`imageID`, `postID`, `image`)
+         VALUES ('$imageID','$postID','$img')";
+                $result = mysqli_query($mysql_con, $query);
+
+                if ($result) return true;
+                else return false;
+            }
+
+            $imgFileLength = ($img != null) ? count($img['name']) : 0;
+            $timestamp = date('Y-m-d H:i:s');
+            $status = 'available'; // Default status
+
+            $query = "INSERT INTO `post`(`postID`, `username`, `colCode`, `caption`, `date`, `timestamp`, `status`) 
+              VALUES (?,?,?,?,?,?,?)";
+
+            $stmt = mysqli_prepare($mysql_con, $query);
+
+            if ($stmt) {
+                // Bind data
+                mysqli_stmt_bind_param($stmt, 'sssssss', $postID, $username, $colCode, $caption, $date, $timestamp, $status);
+                $result = mysqli_stmt_execute($stmt);
+
+                if ($result) {
+                    // If the post has an image/s included
+                    if ($imgFileLength) {
+                        // Store all images one by one
+                        for ($i = 0; $i < $imgFileLength; $i++) {
+                            $fileContent = addslashes(file_get_contents($img['tmp_name'][$i]));
+                            insertImgPost($postID, $fileContent, $mysql_con);
+                        }
+                    }
+
+                    setNewActivity($mysql_con, $adminID, "posted", $postID);
+
+                    // return true;
+                    echo json_encode(array(
+                        "error" => false, "data" => "",
+                        "status" => true, "message" => "Post created successfully"
+                    ));
+                } else {
+                    // return false;
+                    echo json_encode(array("status" => false, "message" => "Something went wrong"));
+                }
+            } else {
+                // return false;
+                echo json_encode(array("status" => "error", "message" => "Something went wrong"));
+            }
+        } else if ($_POST["action"] == "delete") {
+        } else if ($_POST["action"] == "like") {
+        } else if ($_POST["action"] == "comment") {
+        }
+    } else {
+        // create a new post
+        // create_post($_POST["message"]);
+    }
 }
+
+
 
 function get_all_alumni()
 {
--- college/make-post/post.js
+++ college/make-post/post.js
@@ -3,14 +3,128 @@ import { getJSONFromURL, postJSONFromURL } from "../scripts/utils.js";
 // initialize jquery
 $(document).ready(function () {
   updatePostDataTable();
+  bindHandlers();
 
   $('a[href="#make-post"]').on("click", function () {
     setTimeout(function () {
       // remove the loading screen
       $(".loading-row").addClass("hidden");
       updatePostDataTable();
+      bindHandlers();
     }, 1000);
   });
+
+  function bindHandlers() {
+    $("#fileGallery").change(() => {
+      $("#errorMsg").addClass("hidden"); //always set the error message as hidden when changing the file
+      $("#TxtAreaAnnouncement").addClass("h-5/6").removeClass("h-3/6");
+
+      //file input
+      var fileInput = $("#fileGallery");
+      var file = fileInput[0].files[0]; //get the first file that being select
+
+      fileExtension = file.name.split(".").pop().toLowerCase(); //getting the extension of the selected file
+      //checking if the file is based on the extension we looking for
+      if (validExtension.includes(fileExtension)) {
+        var reader = new FileReader();
+        selectedFiles.push(file); // Store the selected file in the array
+        //new image element to be place on the  image container div
+        const imageElement = document.createElement("img");
+
+        const imgPlaceHolder = document.createElement("div");
+        imgPlaceHolder.className = "relative";
+
+        //for button x
+        const xBtn = document.createElement("button");
+        xBtn.innerHTML = "X";
+        xBtn.className =
+          "xBtn absolute h-5 w-5 top-0 text-center right-0 cursor-pointer rounded-full hover:bg-accent hover:text-white hover:font-bold";
+        //remove the image
+        xBtn.addEventListener("click", function (e) {
+          var parent = e.target.parentNode;
+          var index = Array.from(parent.parentNode.children).indexOf(parent); //get a specific index which picture has been remove
+          selectedFiles.splice(index, 1); // Remove the file from the selectedFiles array
+          parent.parentNode.removeChild(parent);
+        });
+
+        // img element
+        imageElement.className = "flex-shrink-0 h-20 w-20 rounded-md m-2";
+        imageElement.setAttribute("id", "reservedPicture" + imageSequence); //to make sure every id is unique
+
+        //add to its corresponding container
+        imgPlaceHolder.appendChild(imageElement);
+        imgPlaceHolder.appendChild(xBtn);
+        imgContPost.appendChild(imgPlaceHolder);
+
+        //assign the image path to the img element
+        reader.onload = function (e) {
+          $("#reservedPicture" + imageSequence).attr("src", e.target.result);
+          $("#imgContPost").removeClass("hidden");
+          $("#TxtAreaAnnouncement").addClass("h-3/6").removeClass("h-5/6"); //make the text area smaller in height
+          imageSequence++;
+        };
+
+        reader.readAsDataURL(file);
+      } else {
+        $("#errorMsg").removeClass("hidden"); //if the file is not based on the img extension we looking for
+      }
+    });
+
+    // on submit of make-post-form
+    $("#make-post-form").on("submit", async function (e) {
+      e.preventDefault();
+      // get the form data
+      const formData = new FormData(this);
+      // formData.append("files", selectedFiles);
+      formData.append("action", "createPost");
+
+      const fileGallery = $("#fileGallery");
+
+      console.log(formData);
+
+      // const response = await
+      // make confirmation with sweet alert
+      const result = await Swal.fire({
+        title: "Confirmation",
+        text: "You are about to post this announcement. Are you sure?",
+        icon: "info",
+        showCancelButton: true,
+      });
+
+      if (result.isConfirmed) {
+        // post the form data
+        postJSONFromURL("./make-post/apiPosts.php", formData).then((data) => {
+          console.log(data);
+          if (data.status == true) {
+            // reset the form
+            $("#make-post-form")[0].reset();
+            // remove the image container
+            $("#imgContPost").addClass("hidden");
+            // remove the selected files
+            // selectedFiles = [];
+            // update the table
+            // updatePostDataTable();
+            // reload the #postTable datatable
+            $("#postTable").DataTable().ajax.reload();
+            // add success alert
+            Swal.fire({
+              title: "Success",
+              text: "Your Post is Successfully Posted!",
+              icon: "success",
+            });
+            // toggle newPostModal checkbox
+            $("#newPostModal").prop("checked", false);
+          } else {
+            Swal.fire({
+              title: "Error",
+              text: "Your Post is not Successfully Posted!",
+              icon: "error",
+            });
+          }
+        });
+      }
+    });
+  }
 
   function updatePostDataTable() {
     // remove the loading screen
